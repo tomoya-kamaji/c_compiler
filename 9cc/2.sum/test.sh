7360428f770506b50de536d41586e1ac3c1c9c99
tomoya <tomoya.kamaji@gmail.com>
2023-10-10 14:23:46 +0900

    シングルリンクリスト

--- 9cc/2.sum/test.sh
+++ 9cc/2.sum/test.sh
@@ -17,7 +17,6 @@ assert() {
   fi
 }
 
-# assert 0 0
-# assert 42 42
-# assert 21 "5+20-4"
-assert 41 " 12 +34-  5 "
+assert 0 0
+assert 42 42
+assert 21 "5+20-4"
--- 9cc/tmp.s
+++ 9cc/tmp.s
@@ -1,6 +1,12 @@
+Token: Kind=TK_NUM, Value=12
+Token: Kind=TK_RESERVED, Symbol=+
+Token: Kind=TK_NUM, Value=34
+Token: Kind=TK_RESERVED, Symbol=-
+Token: Kind=TK_NUM, Value=5
+Token: Kind=TK_EOF
 .global main
 main:
-  mov x0, 5
-  add x0, x0, 20
-  sub x0, x0, 4
+  mov x0, 12
+  add x0, x0, 34
+  sub x0, x0, 5
   ret
